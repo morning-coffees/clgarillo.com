34807957776182d29697a2dfcc3059e7c9835a67
Christian Garillo (Morning-Coffees) <garillo.cl@gmail.com>
2023-05-20 22:29:31 +0800

    Added tsparticles background on random. will add more later

---
+++ src/layouts/ParticleBackground.ts
@@ -0,0 +1,9 @@
+
+import { particles1 }  from "./particles/Particles1";
+import { particles2 }  from "./particles/Particles2";
+import { particles3 }  from "./particles/Particles3";
+import { particles4 }  from "./particles/Particles4";
+
+const particleBackground = [particles1, particles2, particles3];
+
+export default particleBackground;
